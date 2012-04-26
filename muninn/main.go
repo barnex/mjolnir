66df0868f735d1d7b8be2afe83b2c17b2c77b29a
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-04-26 14:13:42 +0200

    start munin auto-configuration tool

---
+++ muninn/main.go
@@ -0,0 +1,24 @@
+package main
+
+import(
+	cu "cuda/driver"
+)
+
+type NodeInfo struct{
+	HostName string
+	NDevice int
+	Devices []Device
+}
+
+type Device struct{
+	Name string
+	TotalMem int64
+}
+
+func main(){
+	var info NodeInfo
+	info.HostName = os.Hostname()
+	NDev := cu.DeviceGetCount()	
+	
+	
+}
