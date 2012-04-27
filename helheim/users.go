c564dfbecb5606ce6d11626c733095a108ba9345
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-04-27 21:11:55 +0200

    nicer users info

--- helheim/users.go
+++ helheim/users.go
@@ -16,6 +16,7 @@ type User struct {
 	share int // Relative group share of the user
 }
 
+// Add new group to global list and return it as well.
 func AddGroup(name string, share int) *Group {
 	group := &Group{name, share, []*User{}}
 	groups = append(groups, group)
@@ -29,10 +30,26 @@ func (g *Group) AddUser(name string, share int) {
 // API func, prints user info.
 func Users(out io.Writer) error {
 	for _, gr := range groups {
-		fmt.Fprint(out, gr)
+		fmt.Fprint(out, gr.name, " (share ", gr.share, "/", TotalGroupShare(), ")\n")
 		for _, usr := range gr.users {
-			fmt.Fprintln(out, "\n\t", usr)
+			fmt.Fprint(out, "\t", usr.name, " (share ", usr.share, "/", gr.TotalUserShare(), ")\n")
 		}
 	}
 	return nil
+}
+
+func TotalGroupShare() int {
+	total := 0
+	for _, gr := range groups {
+		total += gr.share
+	}
+	return total
+}
+
+func (g *Group) TotalUserShare() int {
+	total := 0
+	for _, usr := range g.users {
+		total += usr.share
+	}
+	return total
 }
