2a7570860e69f7e5b2c1f8d15b0f7651e35ee4ac
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-05-09 11:19:03 +0200

    translate fix 2

--- helheim/add.go
+++ helheim/add.go
@@ -3,6 +3,7 @@ package helheim
 import (
 	"errors"
 	"io"
+	"os"
 	"os/user"
 	"path"
 	"strconv"
@@ -59,9 +60,16 @@ func Add(out io.Writer, osUser *user.User, args []string) (err error) {
 	return nil
 }
 
-func TranslatePath(file string)string{
-		if path.IsAbs(file) && strings.HasPrefix(file, translate[0]){
+func TranslatePath(file string) string {
+	wd, err := os.Getwd()
+	if err != nil {
+		return file
+	}
+	if !path.IsAbs(file) {
+		file = wd + file
+		if strings.HasPrefix(file, translate[0]) {
 			return translate[1] + file[len(translate[0]):]
 		}
-		return file
+	}
+	return file
 }
